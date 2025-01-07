e2e065c936c69fb95286510dc1382b2588f49d47
codewithmecoder <yaty1031@gmail.com>
2025-01-07 18:31:48 +0700

    add hash password util

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod config;
 mod dtos;
 mod error;
 mod models;
+mod utils;
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/utils/password.rs
@@ -0,0 +1,48 @@
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
+
+use crate::error::ErrorMessage;
+
+const MAX_PASSWORD_LENGTH: usize = 64;
+
+pub fn hash(password: impl Into<String>) -> Result<String, ErrorMessage> {
+    let password = password.into();
+    if password.is_empty() {
+        return Err(ErrorMessage::EmptyPassword);
+    }
+
+    if password.len() > MAX_PASSWORD_LENGTH {
+        return Err(ErrorMessage::ExceededMaxPasswordLength(MAX_PASSWORD_LENGTH));
+    }
+
+    let salt = SaltString::generate(&mut OsRng);
+
+    let argon2 = Argon2::default();
+    let hashed_password = argon2
+        .hash_password(password.as_bytes(), &salt)
+        .map_err(|_| ErrorMessage::HashingError)?
+        .to_string();
+
+    Ok(hashed_password)
+}
+
+pub fn compare(password: &str) -> Result<bool, ErrorMessage> {
+    if password.is_empty() {
+        return Err(ErrorMessage::EmptyPassword);
+    }
+
+    if password.len() > MAX_PASSWORD_LENGTH {
+        return Err(ErrorMessage::ExceededMaxPasswordLength(MAX_PASSWORD_LENGTH));
+    }
+
+    let argon2 = Argon2::default();
+    let password_hash =
+        PasswordHash::new(&password).map_err(|_| ErrorMessage::InvalidHashFormat)?;
+
+    let password_verifier = argon2
+        .verify_password(password.as_bytes(), &password_hash)
+        .map_or(false, |_| true);
+    Ok(password_verifier)
+}
