b9f00a021eb96d4ccabcd9fd41a4637b20f3bde7
codewithmecoder <yaty1031@gmail.com>
2025-01-07 15:01:33 +0700

    fix format

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-mod models;
 mod config;
-
+mod dtos;
+mod models;
 fn main() {
     println!("Hello, world!");
 }
--- src/models.rs
+++ src/models.rs
@@ -4,34 +4,34 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, sqlx::Type)]
 #[sqlx(type_name = "user_role", rename_all = "lowercase")]
 pub enum UserRole {
-  Admin,
-  Moderator,
-  User,
+    Admin,
+    Moderator,
+    User,
 }
 
 impl UserRole {
-  pub fn to_str(&self) -> &str {
-    match self {
-      UserRole::Admin => "admin",
-      UserRole::Moderator => "moderator",
-      UserRole::User => "user",
+    pub fn to_str(&self) -> &str {
+        match self {
+            UserRole::Admin => "admin",
+            UserRole::Moderator => "moderator",
+            UserRole::User => "user",
+        }
     }
-  }
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, sqlx::FromRow, sqlx::Type)]
 pub struct User {
-  pub id: uuid::Uuid,
-  pub name: String,
-  pub email: String,
-  pub role: UserRole,
-  pub photo: String,
-  pub verified: bool,
-  pub password: String,
+    pub id: uuid::Uuid,
+    pub name: String,
+    pub email: String,
+    pub role: UserRole,
+    pub photo: String,
+    pub verified: bool,
+    pub password: String,
 
-  #[serde(rename = "createdAt")]
-  pub created_at: Option<DateTime<Utc>>,
+    #[serde(rename = "createdAt")]
+    pub created_at: Option<DateTime<Utc>>,
 
-  #[serde(rename = "updatedAt")]
-  pub updated_at: Option<DateTime<Utc>>,
+    #[serde(rename = "updatedAt")]
+    pub updated_at: Option<DateTime<Utc>>,
 }
