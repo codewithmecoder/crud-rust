418b5fefbd5ebee29e4d862bb5f84c9c66d0360b
codewithmecoder <yaty1031@gmail.com>
2025-01-07 11:42:36 +0700

    add user model

--- src/models.rs
+++ src/models.rs
@@ -1,1 +1,37 @@
-use chrono::
+use chrono::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, sqlx::Type)]
+#[sqlx(type_name = "user_role", rename_all = "lowercase")]
+pub enum UserRole {
+  Admin,
+  Moderator,
+  User,
+}
+
+impl UserRole {
+  pub fn to_str(&self) -> &str {
+    match self {
+      UserRole::Admin => "admin",
+      UserRole::Moderator => "moderator",
+      UserRole::User => "user",
+    }
+  }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, sqlx::FromRow, sqlx::Type)]
+pub struct User {
+  pub id: uuid::Uuid,
+  pub name: String,
+  pub email: String,
+  pub role: UserRole,
+  pub photo: String,
+  pub verified: bool,
+  pub password: String,
+
+  #[serde(rename = "createdAt")]
+  pub created_at: Option<DateTime<Utc>>,
+
+  #[serde(rename = "updatedAt")]
+  pub updated_at: Option<DateTime<Utc>>,
+}
